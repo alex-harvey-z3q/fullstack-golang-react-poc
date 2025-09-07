4b4b102d5bbaf208e6518d66818d1036e22e295e
Alex Harvey <alex.harvey.z3q@gmail.com>
2025-09-07 22:06:28 +1000

    Better comment

--- services/tasks/cmd/server/main.go
+++ services/tasks/cmd/server/main.go
@@ -14,10 +14,19 @@ func main() {
 	// Read config from services/tasks/internal/config/config.go.
 	cfg := config.Load()
 
-	// The Repository design pattern is a structural pattern that abstracts data access,
-	// providing a centralised way to manage data operations. By separating the data layer
-	// from business logic, it enhances code maintainability, testability, and flexibility,
-	// making it easier to work with various data sources in an application.
+	// The Repository design pattern
+	// (https://martinfowler.com/eaaCatalog/repository.html)
+	//
+	// "A Repository mediates between the domain and data mapping layers, acting like
+	// an in-memory domain object collection. Client objects construct query specifications
+	// declaratively and submit them to Repository for satisfaction. Objects can be added
+	// to and removed from the Repository, as they can from a simple collection of objects,
+	// and the mapping code encapsulated by the Repository will carry out the appropriate
+	// operations behind the scenes. Conceptually, a Repository encapsulates the set of
+	// objects persisted in a data store and the operations performed over them, providing
+	// a more object-oriented view of the persistence layer. Repository also supports the
+	// objective of achieving a clean separation and one-way dependency between the domain
+	// and data mapping layers."
 	//
 	// This reads from services/tasks/internal/tasks/repo.go. It creates a pgxpool to
 	// talk to the Postgres database.
